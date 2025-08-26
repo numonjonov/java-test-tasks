7253fbcb0ad408b139afcd63accdf59e347d462a
Numonjonov <numonjonovpahlavon1@gmail.com>
2025-08-26 23:52:44 +0500

    Added all 4 tasks with Main.java

---
+++ src/two_sums/Main.java
@@ -0,0 +1,7 @@
+package two_sums;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/word_counter/Main.java
@@ -0,0 +1,7 @@
+package word_counter;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
